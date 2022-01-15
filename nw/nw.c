c7be9086075008e1227990e64f4d9b3941770920
markpal <marekp1980@gmail.com>
2022-01-15 08:39:15 +0100

    Update nw.c

--- nw/nw.c
+++ nw/nw.c
@@ -34,7 +34,30 @@ int s(unsigned char x, unsigned char z){
 
 
 #include "mem.h"
-#include "sw_oryg.h"
+
+
+void sw_seq()
+{
+int i,j,k;
+
+printf("- oryginal code - \n\n");
+
+#pragma scop
+for (i=1; i <=N; i++)
+    for (j=1; j <=N; j++){
+    // Block S
+        m1[i][j] = INT_MIN;
+        for (k=1; k <=i; k++)
+            m1[i][j] = MAX(m1[i][j] ,H[i-k][j] + W[k]);
+        m2[i][j] = INT_MIN;
+        for (k=1; k <=j; k++)
+            m2[i][j] = MAX(m2[i][j] ,H[i][j-k] + W[k]);
+        H[i][j] = MAX(0, MAX( H[i-1][j-1] + s(a[i], b[i]), MAX(m1[i][j], m2[i][j])));
+    }
+#pragma endscop
+
+
+}
 
 
 int main(int argc, char *argv[]){
